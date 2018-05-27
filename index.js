3f83f7acda025a953399ea93907e93aa50b4cc60
yinm <yinm.cminor9@gmail.com>
2018-05-27 17:10:16 +0900

    Add handle

--- index.js
+++ index.js
@@ -95,3 +95,79 @@ proto.use = (route, fn) => {
 
   return this
 }
+
+/**
+ * Handle server requests, punting them down
+ * the middleware stack.
+ *
+ * @private
+ */
+proto.handle = (req, res, out) => {
+  let index = 0
+  let protohost = getProtohost(req.url) || ''
+  let removed = ''
+  let slashAdded = false
+  let stack = this.stack
+
+  // final function handler
+  let done = out || finalhandler(req, res, {
+    env: env,
+    onerror: logerror,
+  })
+
+  // store the original URL
+  req.originalUrl = req.originalUrl || req.url
+
+  function next(err) {
+    if (slashAdded) {
+      req.url = req.url.substr(1)
+      slashAdded = false
+    }
+
+    if (removed.length !== 0) {
+      req.url = protohost + removed + req.url.substr(protohost.length)
+      removed = ''
+    }
+
+    //next callback
+    let layer = stack[index++]
+
+    // all done
+    if (!layer) {
+      defer(done, err)
+      return
+    }
+
+    // route data
+    let path = parseUrl(req).pathname || '/'
+    let route = layer.route
+
+    // skip this layer if the route doesn't match
+    if (path.toLowerCase().substr(0, route.length) !== route.toLowerCase()) {
+      return next(err)
+    }
+
+    // skip if route match does not border "/", ".", or end
+    let c = path.length > route.length && path[route.length]
+    if (c && c !== '/' && c !== '.') {
+      return next(err)
+    }
+
+    // trim off the part of the url that matches the route
+    if (route.length !== 0 && route !== '/') {
+      removed = route
+      req.url = protohost + req.url.substr(protohost.length + removed.length)
+
+      // ensure leading slash
+      if (!protohost && req.url[0] !== '/') {
+        req.url = '/' + req.url
+        slashAdded = true
+      }
+    }
+
+    // call the layer handle
+    call(layer.handle, route, err, req, res, next)
+  }
+
+  next()
+}
