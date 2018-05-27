6222ac56420b51605bb1277be7be7a015e4274af
yinm <yinm.cminor9@gmail.com>
2018-05-27 17:57:09 +0900

    Add should inherit from event emitter

---
+++ test/server.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+const connect = require('..')
+const http = require('http')
+const rawrequest = require('./support/rawagent')
+const request = require('supertest')
+
+describe('app', () => {
+  let app
+
+  beforeEach(() => {
+    app = connect()
+  })
+
+  it('should inherit from event emitter', (done) => {
+    app.on('foo', done)
+    app.emit('foo')
+  })
+})
