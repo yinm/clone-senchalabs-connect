d6c30b8bce02dc9f1b706a429db70060b0630fe7
yinm <yinm.cminor9@gmail.com>
2018-05-27 16:58:12 +0900

    Add use

--- index.js
+++ index.js
@@ -43,3 +43,55 @@ function createServer() {
   app.stack = []
   return app
 }
+
+/**
+ * Utilize the given middleware `handle` on the given `route`,
+ * defaulting to _/_. This "route" is the mount-point for the
+ * middleware, when given a value other than _/_ the middleware
+ * is only effective when that segment is present in the request's
+ * pathname.
+ *
+ * For example if we were to mount a function at _/admin_, it would
+ * be invoked on _/admin_, and _/admin/settings_, however it would
+ * not be invoked for _/_, or _posts_.
+ *
+ * @param {String|Function|Server} route callback or server
+ * @param {Function|Server} fn callback or server
+ * @returns {Server} for chaining
+ * @public
+ */
+proto.use = (route, fn) => {
+  let handle = fn
+  let path = route
+
+  // default route to '/'
+  if (typeof route !== 'string') {
+    handle = route
+    path = '/'
+  }
+
+  // wrap sub-apps
+  if (typeof handle.handle === 'function') {
+    let server = handle
+    server.route = path
+    handle = (req, res, next) => {
+      server.handle(req, res, next)
+    }
+  }
+
+  // wrap vanilla http.Servers
+  if (handle instanceof http.Server) {
+    handle = handle.listeners('request')[0]
+  }
+
+  // strip trailing slash
+  if (path[path.length - 1] === '/') {
+    path = path.slice(0, -1)
+  }
+
+  // add the middleware
+  debug('use %s %s', path || '/', handle.name || 'annonymous')
+  this.stack.push({ route: path, handle: handle })
+
+  return this
+}
